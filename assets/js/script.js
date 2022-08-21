05f62f2862df322c7b8cd5c0ab61c030f21dd448
Jean de Villiers <jeandevilliers@me.com>
2022-08-21 13:39:37 +0000

    Show or hide mobile header & buttons, based on device used

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
 // Detect Mobile Phone
-if ((window.matchMedia('(max-width: 414px)')) || (window.matchMedia('(max-width: 915px)')).matches) {
-    console.log("Mobile Phone");
-    moveSiteTitle();
-} else {
-    console.log("Desktop/Tablet");
+const mediaQueryList = [
+    window.matchMedia('(max-width: 414px)'),
+    window.matchMedia('(max-width: 915px)')
+]
+
+for (let mediaQuery of mediaQueryList) {
+
+    if (mediaQuery.matches) {
+        console.log("Mobile Phone");
+        document.body.classList.add('mobile-phone');
+        document.getElementById("mobileHeader").style.display = 'flex';
+        moveSiteTitle();
+    } else {
+        console.log("Desktop/Tablet");
+        document.getElementById("mobileHeader").style.display = 'none';
+    }
 }
 
 // Move site title from control area to mobile header
 function moveSiteTitle() {
     const sourceElement = document.getElementById("siteTitle");
-    const targetElement = document.getElementById("mobileHeader");
+    const targetElement = document.getElementById("mobileButtons");
 
-    targetElement.appendChild(sourceElement);
+    targetElement.before(sourceElement);
 }

318d8a2615a1469775a5d6ebd99b427975544453
Jean de Villiers <jeandevilliers@me.com>
2022-08-22 11:11:50 +0000

    Simply javascript for handling mobile phone header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,32 @@
-// Detect Mobile Phone
+// Set global variables
 const mediaQueryList = [
     window.matchMedia('(max-width: 414px)'),
     window.matchMedia('(max-width: 915px)')
-];
+    ];
+const mobileHeader = document.getElementById("mobileHeader");
+const siteTitle = document.getElementById("siteTitle");
+const mobileButtons = document.getElementById("mobileButtons");
+
+// Detect mobile phone & display mobile header
+window.addEventListener('load', function() {
 
 for (let mediaQuery of mediaQueryList) {
 
     if (mediaQuery.matches) {
-        console.log("Mobile Phone");
+
+        // add mobile-phone CSS body class
         document.body.classList.add('mobile-phone');
-        document.getElementById("mobileHeader").style.display = 'flex';
-        moveSiteTitle();
+
+        // Move site title into mobile header, above buttons
+        mobileButtons.before(siteTitle);
+
+        // Display mobile header
+        mobileHeader.style.display = 'flex';
     } else {
-        console.log("Desktop/Tablet");
-        document.getElementById("mobileHeader").style.display = 'none';
+
+        // Hide mobile header on desktop & tablet
+        mobileHeader.style.display = 'none';
     }
 }
 
-// Move site title from control area to mobile header
-function moveSiteTitle() {
-    const sourceElement = document.getElementById("siteTitle");
-    const targetElement = document.getElementById("mobileButtons");
-
-    targetElement.before(sourceElement);
-}
+});

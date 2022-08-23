6f3748f2d138f5f0442c79669d51799ede72d14c
Jean de Villiers <jeandevilliers@me.com>
2022-08-23 15:37:57 +0000

    Fix button out-of-scope variables using forEach and querySelectorAll

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,22 +7,22 @@ const mediaQueryList = [
     window.matchMedia('(max-width: 414px)'),
     window.matchMedia('(max-width: 915px)')
     ];
+
 const mobileHeader = document.getElementById("mobileHeader");
 const siteTitle = document.getElementById("siteTitle");
 const mobileButtons = document.getElementById("mobileButtons");
+const helpButtons = document.querySelectorAll(".help-btn");
+const closeButtons = document.querySelectorAll(".close-btn");
 const controlArea = document.getElementById("controls");
 const mobileControlBtn = document.getElementById("controlAreaBtn");
 const helpContent = document.getElementById("helpContent");
 const drawer = document.getElementById("offCanvas");
-const drawerCloseBtn = document.getElementById("drawerCloseBtn");
 const keySelect = document.getElementById("keySelect");
 
 
 // Detect mobile phone & handle element display
-    window.addEventListener('load', function() {
-
+window.addEventListener('load', function() {
     for (let mediaQuery of mediaQueryList) {
-
         if (mediaQuery.matches) {
 
             // add mobile-phone CSS body class
@@ -35,15 +35,13 @@ const keySelect = document.getElementById("keySelect");
             mobileHeader.style.display = 'flex';
 
         } else {
-
             keySelect.before(helpBtn);
-
         }
-
     }
+});
 
-    });
 
+// Button Event Listeners
 
 // Mobile phone control area button
 mobileControlBtn.addEventListener("click", function() {
@@ -52,37 +50,30 @@ mobileControlBtn.addEventListener("click", function() {
     controlArea.style.display = "flex";
     drawer.classList.add("in");
     drawerCloseBtn.after(controlArea);
-    
-    
-    drawerCloseBtn.addEventListener("click", function() {
-        drawer.classList.remove("in");
-        drawerCloseBtn.after('');
+
+});
+
+// Help button
+helpButtons.forEach(helpBtn => {
+
+    helpBtn.addEventListener("click", function() {
+        console.log("Help button clicked");
+        helpBtn.classList.add("active");
+        helpContent.style.cssText = ("display: flex; z-index: 10;");
+        drawer.classList.add("in");
+        drawerCloseBtn.after(helpContent);
     });
 
 });
 
 
+// Drawer close button
+closeButtons.forEach(closeBtn => {
 
-// Help buttons
-let helpBtnCount = document.getElementsByClassName("help-btn");
-for (let i = 0; i < helpBtnCount.length; i++) {
-
-    console.log(helpBtnCount[i]);
-    let helpBtn = helpBtnCount[i];
-
-    helpBtn.addEventListener("click", function() {
-        this.classList.add("active");
-        helpContent.style.cssText = ("display: flex; z-index: 10;");
-        drawer.classList.add("in");
-        drawerCloseBtn.after(helpContent);
-        
-        
-        drawerCloseBtn.addEventListener("click", function() {
-            drawer.classList.remove("in");
-            drawerCloseBtn.after('');
-        });
-
+    closeBtn.addEventListener("click", function() {
+        console.log("Close button clicked");
+        drawer.classList.remove("in");
+        helpBtn.classList.remove("active");
     });
 
-}
-
+});

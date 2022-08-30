3cf3e4c0f0d845650f8edcfa6f3d2416f849636e
Jean de Villiers <jeandevilliers@me.com>
2022-08-30 13:58:32 +0000

    Comment out code while troubleshooting button behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,21 +10,18 @@ const mediaQueryList = [
 
 const mobileHeader = document.getElementById("mobileHeader");
 const siteTitle = document.getElementById("siteTitle");
-const mobileButtons = document.getElementById("mobileButtons");
 const helpButtons = document.querySelectorAll(".help-btn");
-const closeButtons = document.querySelectorAll(".close-btn");
 const controlArea = document.getElementById("controls");
 const mobileHeaderControlsBtn = document.getElementById("mobileHeaderControlsBtn");
 const helpContent = document.getElementById("helpContent");
-const drawer = document.getElementById("offCanvas");
-const keySelectButtons = document.querySelectorAll(".key-select");
-const selectedKeyDisplay = document.getElementById("selectedKeyDisplay");
 
 
 // Detect mobile phone & handle element display
 window.addEventListener('load', function() {
     for (let mediaQuery of mediaQueryList) {
         if (mediaQuery.matches) {
+
+            let mobileButtons = document.getElementById("mobileButtons");
 
             // add mobile-phone CSS body class
             document.body.classList.add('mobile-phone');
@@ -45,57 +42,106 @@ window.addEventListener('load', function() {
 // Mobile phone control area button
 mobileHeaderControlsBtn.addEventListener("click", function() {
     mobileHeaderHelpBtn.classList.remove("active");
-    helpContent.style.display = "none";
     controlArea.style.display = "flex";
-    drawer.classList.add("in");
-    drawerCloseBtn.after(controlArea);
+    controlArea.classList.add("show");
 
 });
 
 // Help button
-helpButtons.forEach(helpButton => {
+// helpButtons.forEach(helpButton => {
 
-    helpButton.addEventListener("click", function() {
-        console.log("Help button clicked");
-        mobileHeaderHelpBtn.classList.add("active");
-        controlArea.style.display = "none";
-        helpContent.style.cssText = ("display: flex; z-index: 10;");
-        drawer.classList.add("in");
-        drawerCloseBtn.after(helpContent);
-    });
+//     helpButton.addEventListener("click", function() {
+//         console.log("Help button clicked");
+//         mobileHeaderHelpBtn.classList.add("active");
+//         controlArea.style.display = "none";
+//         helpContent.style.cssText = ("display: flex; z-index: 10;");
+//         drawer.classList.add("in");
+//         drawerCloseBtn.after(helpContent);
+//     });
 
-});
+// });
 
 // Drawer close button
-closeButtons.forEach(closeBtn => {
+// let closeButtons = document.querySelectorAll(".close-btn");
+// closeButtons.forEach(closeBtn => {
 
-    closeBtn.addEventListener("click", function() {
-        console.log("Close button clicked");
-        drawer.classList.remove("in");
-        mobileHeaderHelpBtn.classList.remove("active");
-    });
+//     closeBtn.addEventListener("click", function() {
+//         console.log("Close button clicked");
+//         drawer.classList.remove("in");
+//         mobileHeaderHelpBtn.classList.remove("active");
+//     });
 
-});
+// });
 
 // Key select button
-keySelectButtons.forEach(keySelectButton => {
+let buttons = document.getElementsByTagName("button");
+console.log(buttons);
 
-    document.addEventListener("click", function(event) {
 
-        for (keySelectButton of keySelectButtons) {
+// var keySelectButtonsList = keySelectButtons.length;
+// for (let i = 0; i < keySelectButtonsList; i++) {
+//     console.log(keySelectButtons[i].value);
+//     var keySelectButton;
+// }
 
-            // Remove active css class from non-selected key select buttons
-            keySelectButton.classList.remove("active");
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "controls") {
+            console.log("Controls button clicked");
+            helpContent.style.display = "none";
+        } else if (this.getAttribute("data-type") === "close") {
+            console.log("Close button clicked");
+            drawerOpenClose();
+        } else if (this.getAttribute("data-type") === "help") {
+            console.log("Help button clicked");
+            drawerOpenClose();
+            mobileHeaderHelpBtn.classList.remove("active");
+//     helpContent.style.display = "none";
+//     controlArea.style.display = "flex";
+//     drawer.classList.add("in");
+//     drawerCloseBtn.after(controlArea);
+        }
+    });
+}
+
+    // document.addEventListener("click", function() {
+
+    //     for (let keySelectButton of keySelectButtons) {
+
+    //         // Remove active css class from non-selected key select buttons
+    //         keySelectButton.classList.remove("active");
+    //     }
+
+    //     // Add active css class to clicked/tapped key select button
+    //     keySelectButton.classList.add("active");
+    //     console.log(this.target.value);
+
+    //     // Update selected key display with the clicked/tapped button text
+    //     let buttonText = keySelectButton.target.value;
+    //     selectedKeyDisplay.classList.remove("ghosted");
+    //     selectedKeyDisplay.innerText = buttonText;
+    // });
+
+
+    /**
+     * Fill the selected key display field with
+     * the value from the selected key button
+     */
+    function displaySelectedKeyValue() {
+        let selectedKeyDisplay = (document.getElementById("selectedKeyDisplay").innerText);
+    }
+
+
+    function drawerOpenClose() {
+        let drawer = document.getElementById("offCanvas");
+        if (drawer.classList.contains("in")) {
+            drawer.classList.remove("in");
+        } else {
+            drawer.classList.add("in");
         }
 
-        // Add active css class to clicked/tapped key select button
-        event.target.classList.add("active");
-        console.log(event.target.innerText);
+    }
 
-        // Update selected key display with the clicked/tapped button text
-        let buttonText = event.target.innerText;
-        selectedKeyDisplay.innerText = buttonText;
-    });
 
-});
+
 
